4380e97f81d58d86ec4a33fac44c81b7ef84ff05
MartiSvedas <90530390+MartiSvedas@users.noreply.github.com>
2023-05-20 20:44:26 -0300

    caminos

--- ServicioCaminos.java
+++ ServicioCaminos.java
@@ -1,7 +1,7 @@
 package TPEspecial;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import java.util.Iterator;
 
 
@@ -10,12 +10,14 @@ public class ServicioCaminos<T> {
 	private int origen;
 	private int destino;
 	private int lim;
+	private HashMap <Arco<T>,Boolean> visitado;
 	
 	public ServicioCaminos(Grafo<T> g ,int origen, int destino, int lim) {
 		this.grafo = g;
 		this.origen=origen;
 		this.destino=destino;
 		this.lim=lim;
+		this.visitado= new HashMap<>();
 	}
 	
 	public List<List<Integer>> caminos(){
